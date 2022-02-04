9a2f54f54c3c8f4e8f1bf1316f1e9373cea1be33
Quantum-K9 <97434538+Quantum-K9@users.noreply.github.com>
2022-02-04 10:31:27 +0800

    Create shower-line.py

---
+++ shower-line.py
@@ -0,0 +1,31 @@
+
+from itertools import permutations as p
+
+g = []
+for i in range(5):
+    g.append( [ int(x) for x in input().split() ] )
+
+
+listt = [ 0, 1, 2, 3, 4 ]
+
+allPossibleOptions = list( p(listt) ) 
+
+answer = 0
+
+for option in allPossibleOptions:
+
+    score = 0
+
+    #compute for the score
+    for frontPersonInLine in range(0,5):
+
+        for pair in range( frontPersonInLine, 5, 2 ):
+
+            if pair < 4:
+                score = score + g[ option[ pair ]][ option[ pair+1] ] 
+                score = score + g[ option[ pair+1]][option[pair] ] 
+
+
+    answer = max( answer, score ) 
+
+print(answer)
