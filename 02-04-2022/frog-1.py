07bfec15b02e036c801dc5d474e0ae7195325aaa
Quantum-K9 <97434538+Quantum-K9@users.noreply.github.com>
2022-02-04 18:11:37 +0800

    Create frog-1.py

---
+++ 02-04-2022/frog-1.py
@@ -0,0 +1,33 @@
+
+import sys
+sys.setrecursionlimit( 1000000 )
+
+n = int( input() )
+h = [ int(x) for x in input().split() ]
+
+memo = [-1]*(n+1)
+
+def dp(i): # the cheapest way to get from stone i tp stone n --> dp(1)
+
+    if memo[i] != -1:
+        return memo[i]
+    
+    if i == n-1:
+        return 0
+
+    if i == n-2:
+        return abs( h[i]-h[i+1] )
+
+    #general case: min( cost( stone i to stone i+1 ) + dp(i+1),
+                    #   cost( stone i to stone i+2 ) + dp(i+2))
+
+    answer = min( abs(h[i]-h[i+1]) + dp(i+1),
+                  abs(h[i]-h[i+2]) + dp(i+2) )
+
+    memo[i] = answer
+    return answer
+
+print( dp(0) )
+
+# 1 2 3 4 5 .... n
+# 0 1 2 3 4 .... n-1
