55463f0aab931af185e1b06abd0402097e969b5f
Quantum-K9 <97434538+Quantum-K9@users.noreply.github.com>
2022-02-04 23:18:35 +0800

    Create frog-2-bottom-up.py

---
+++ 02-04-2022/frog-2-bottom-up.py
@@ -0,0 +1,25 @@
+
+n,k = [ int(x) for x in input().split() ]
+h = [ int(x) for x in input().split() ]
+
+memo = [-1]*(n+1)
+
+# memo[i] is the cheapest way to get from stone i to stone n --> dp(1)
+
+memo[n-1] = 0 #adjust since we're using 0-based indexing
+
+for i in range( n-2, -1, -1 ): #loop instead of recursion
+    
+    answer = abs(h[i]-h[i+1]) + memo[i+1] #base value
+    
+    for stepSize in range(2, k+1):
+
+        if i+stepSize > n-1:
+            break
+    
+        answer = min( answer, abs( h[i]-h[i+stepSize] ) + memo[ i + stepSize ] )
+
+    memo[i] = answer
+
+print( memo[0] )
+    
